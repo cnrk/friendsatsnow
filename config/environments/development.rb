f52ad8a16b1fa534d45b8c66f00f65300ed775a4
Christian Rijke <crijke@gmail.com>
2011-10-12 21:56:08 +0200

    fixed mailer template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,18 @@ Friendsatsnow::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # action_mailer configuration
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto:  true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,18 @@ Friendsatsnow::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # action_mailer configuration
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'friendsatsnow.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto:  true
+  }
 end
