56d38a04f1da921e5694131b058c57d2606ffd66
Christian Rijke <crijke@gmail.com>
2012-11-07 19:48:30 +0100

    configure test mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,19 @@ Friendsatsnow::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # action_mailer configuration
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto:  true
+  }
 end
+
